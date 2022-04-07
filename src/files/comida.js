0611ec1bfdd3b548a15479ba73341378f60aec09
Milagros Bogliano <milagrosbogliano@hotmail.com>
2022-04-07 16:21:40 -0300

    Desafio normalizar

--- src/files/comida.js
+++ src/files/comida.js
@@ -10,5 +10,29 @@
         "name": "Copos con dulce de leche",
         "ingredients": "cholocate, dulce de leche...",
         "steeps": "dejar los conitos bañados de chocolate enfriar en la heladera"
+    },
+    {
+        "id": 3,
+        "name": "Chocolate relleno con dulce de leche",
+        "ingredients": "cholocate, dulce de leche...",
+        "steeps": "chocolate con mucho relleno de dulce de leche"
+    },
+    {
+        "id": 4,
+        "name": "Copos de dulce de leche y coco",
+        "ingredients": "coco, dulce de leche...",
+        "steeps": "Tirar el coco arriba del dulce de leche"
+    },
+    {
+        "id": 5,
+        "name": "Dulce de leche",
+        "ingredients": "Leche...",
+        "steeps": "Dejar la leche hervir"
+    },
+    {
+        "id": 6,
+        "name": "Bizcochuelo de vainilla",
+        "ingredients": "Harina Leudante, esencia de vainilla, huevos...",
+        "steeps": "Hacer la mezcla y meterlo al horno"
     }
  ]
